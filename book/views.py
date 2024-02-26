615068b043360347a469ccf639779fdbd98ea3e9
Muhammad <muhamamdmamajonov@gmail.com>
2024-02-26 14:10:30 +0500

    book_list api_view

--- book/views.py
+++ book/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def books_list(request):
+    books = Book.objects.all()
+    data = []
+    for book in books:
+        data.append(
+            {
+                "title": book.title,
+                "description": book.description,
+                "author": book.author.name,
+                # "genre": book.genre.first(),
+                # "tag": book.tag,
+                "price": book.price,
+                "image": book.image.url
+            }
+        )
+    return Response(data=data, status=200)
